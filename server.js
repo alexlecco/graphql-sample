9278f826440a94bb6521afcc8a06756be7941738
alexlecco <alexlecco24@gmail.com>
2021-03-24 23:25:42 -0300

    add books field for authors

--- server.js
+++ server.js
@@ -57,6 +57,11 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
     name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) =>
+        books.filter((book) => book.authorId === author.id),
+    },
   }),
 });
 
